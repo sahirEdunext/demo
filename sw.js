a42b897306b2518ca957d526d7540ead99bbf998
sahirEdunext <88317246+sahirEdunext@users.noreply.github.com>
2021-08-07 13:19:23 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/example.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+  });
+
+  self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+  
+    if (action === 'close') {
+      notification.close();
+    } else {
+      clients.openWindow('https://www.example.com');
+      notification.close();
+    }
+  });
